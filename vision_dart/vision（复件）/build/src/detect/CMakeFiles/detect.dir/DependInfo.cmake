7d83550f8b8e053de548299c41b4abd0ab7c1ef3
aubrey <aubreyyze@gmail.com>
2025-04-02 17:04:20 +0800

    将文件通过ssh上传给github

---
+++ vision_dart/vision（复件）/build/src/detect/CMakeFiles/detect.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aubrey/CODE/vision_dart/vision（复件）/src/detect/src/Detect.cpp" "src/detect/CMakeFiles/detect.dir/src/Detect.cpp.o" "gcc" "src/detect/CMakeFiles/detect.dir/src/Detect.cpp.o.d"
+  "/home/aubrey/CODE/vision_dart/vision（复件）/src/detect/src/openvino/OpenvinoInfer.cpp" "src/detect/CMakeFiles/detect.dir/src/openvino/OpenvinoInfer.cpp.o" "gcc" "src/detect/CMakeFiles/detect.dir/src/openvino/OpenvinoInfer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
